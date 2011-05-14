cc877992e74bd6d16ad1107c93c4322ff3fe2a62
Gonzalo <Gonzalo@localhost>
2011-05-14 15:07:04 -0300

    Trabajo terminado, realiza un pingpong con 10 mensajes de distintos tama?os con la cantidad de env?os introducido por el usuario.
    --
    user: Gonzalo
    branch 'default'
    changed pingpong.c
    changed pingpong.h
    removed pingpong2.c
    removed pingpong2.h

--- pingpong.c
+++ pingpong.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
+#include <sys/utsname.h>
 #include "pingpong.h"
 
 int main(int argc, char **argv)
 {
 	int proc_id, n_procs, i, envios;
 	int buffer0[SIZE0], buffer1[SIZE1], buffer2[SIZE2], buffer3[SIZE3], buffer4[SIZE4];
-//	double buffer5[SIZE5], buffer6[SIZE6], buffer7[SIZE7], buffer8[SIZE8], buffer9[SIZE9];
-	double t_inicio, t_transcurrido;
+	int buffer5[SIZE5], buffer6[SIZE6], buffer7[SIZE7], buffer8[SIZE8], buffer9[SIZE9];
 	MPI_Status estado;
 
 	/* Inicio del entorno MPI */
@@ -34,11 +34,11 @@ int main(int argc, char **argv)
 		printf("## Cálculo de RTT / Envío Maestro-Esclavo\n\n");
 		printf("## Total de procesadores: %d\n", n_procs);
 
+		obtener_info_sist();
 		/* Leer de stdin la cantidad de envíos a hacer */
 		do
 		{
 			printf("%s", MENU_ENVIOS);
-			printf("## ");
 			scanf("%d", &envios);
 		} while ((envios != 1) && (envios != 2) && (envios != 3));
 
@@ -67,118 +67,30 @@ int main(int argc, char **argv)
 		printf("## \tSIZE2 = %d\n", SIZE2);
 		printf("## \tSIZE3 = %d\n", SIZE3);
 		printf("## \tSIZE4 = %d\n", SIZE4);
-/*		printf("## \tSIZE5 = %d\n", SIZE5);
+		printf("## \tSIZE5 = %d\n", SIZE5);
 		printf("## \tSIZE6 = %d\n", SIZE6);
 		printf("## \tSIZE7 = %d\n", SIZE7);
 		printf("## \tSIZE8 = %d\n", SIZE8);
 		printf("## \tSIZE9 = %d\n", SIZE9);
-*/		printf("\n");
+		printf("\n");
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	if (proc_id == MASTER)
 	{
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer0[0] = i;
-			MPI_Send(buffer0, SIZE0, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer0, SIZE0, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 0: %g\n", t_transcurrido);
-
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer1[0] = i;
-			MPI_Send(buffer1, SIZE1, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer1, SIZE1, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 1: %g\n", t_transcurrido);
-	
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer2[0] = i;
-			MPI_Send(buffer2, SIZE2, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer2, SIZE2, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 2: %g\n", t_transcurrido);
-
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer3[0] = i;
-			MPI_Send(buffer3, SIZE3, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer3, SIZE3, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 3: %g\n", t_transcurrido);
-
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer4[0] = i;
-			MPI_Send(buffer4, SIZE4, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer4, SIZE4, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 4: %g\n", t_transcurrido);
-/*
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer5[0] = i;
-			MPI_Send(buffer5, SIZE5, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer5, SIZE5, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 5: %g\n", t_transcurrido);
-
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer6[0] = i;
-			MPI_Send(buffer6, SIZE6, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer6, SIZE6, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 6: %g\n", t_transcurrido);
-
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer7[0] = i;
-			MPI_Send(buffer7, SIZE7, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer7, SIZE7, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 7: %g\n", t_transcurrido);
-
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer8[0] = i;
-			MPI_Send(buffer8, SIZE8, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer8, SIZE8, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 8: %g\n", t_transcurrido);
-
-		t_inicio = MPI_Wtime();
-		for (i = 0; i < envios; ++i)
-		{
-			buffer9[0] = i;
-			MPI_Send(buffer9, SIZE9, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
-			MPI_Recv(buffer9, SIZE9, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
-		}
-		t_transcurrido = MPI_Wtime() - t_inicio;
-		printf("## tiempo transcurrido para el envío 9: %g\n", t_transcurrido);
-*/
+		printf("## | tam (b) | envios | t_total (seg) | t/envio (s)  |     kB/s   |\n");
+		printf("## |---------|--------|---------------|--------------|------------|\n");
+		master_func(SIZE0, envios, estado);
+		master_func(SIZE1, envios, estado);
+		master_func(SIZE2, envios, estado);
+		master_func(SIZE3, envios, estado);
+		master_func(SIZE4, envios, estado);
+		master_func(SIZE5, envios, estado);
+		master_func(SIZE6, envios, estado);
+		master_func(SIZE7, envios, estado);
+		master_func(SIZE8, envios, estado);
+		master_func(SIZE9, envios, estado);
+		printf("## |_________|________|_______________|______________|____________|\n");
 	}
 	else
 	{
@@ -220,7 +132,7 @@ int main(int argc, char **argv)
 			buffer4[0] += 1;
 			MPI_Send(buffer4, SIZE4, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
 		}
-/*		for(i = 0; i < envios; ++i)
+		for(i = 0; i < envios; ++i)
 		{
 			MPI_Recv(buffer5, SIZE5, MPI_INT, MASTER, 0, MPI_COMM_WORLD, &estado);
 			buffer5[0] += 1;
@@ -254,10 +166,53 @@ int main(int argc, char **argv)
 			buffer9[0] += 1;
 			MPI_Send(buffer9, SIZE9, MPI_INT, MASTER, 0, MPI_COMM_WORLD);
 		}
-*/	}
+	}
 
-	/* Terminate MPI */
+	/* Terminar entorno */
 	MPI_Finalize ();
 	exit(EXIT_SUCCESS);
 }
 
+/* Utiliza la llamada uname y un struct predefinido para obtener info del sistema */
+void obtener_info_sist()
+{
+	int dont_care, mpi_subversion, mpi_version;
+	struct utsname info;
+
+	uname(&info);
+	dont_care = MPI_Get_version(&mpi_version,&mpi_subversion);
+
+	printf("## Arquitectura: \t%s\n",info.machine);
+	printf("## Sistema: \t%s\n", info.sysname);
+	printf("## Release : \t%s\n",info.release);
+	printf("## Version : \t%s\n",info.version);
+	printf("## MPI Version : \t%-d.%-d\n",mpi_version,mpi_subversion);
+	printf("\n");
+	return;
+}
+
+/* Función del maestro que utiliza para mandar los datos, y recibir los que envía el esclavo */
+/* También calcula los tiempos de demora, y tasa de envío, e imprime los resultados en pantalla */
+void master_func(int size, int envios, MPI_Status estado)
+{
+	int i, buffer[size];
+	double t_transcurrido;
+	int tamanio;
+	double tasa;
+	double t_inicio = MPI_Wtime();
+
+	for (i = 0; i < envios; ++i)
+	{
+		buffer[0] = i;
+		MPI_Send(buffer, size, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD);
+		MPI_Recv(buffer, size, MPI_INT, ESCLAVO, 0, MPI_COMM_WORLD, &estado);
+	}
+	t_transcurrido = MPI_Wtime() - t_inicio;
+
+	tamanio = size*32; 					/* Tamaño del dato en bit */
+	
+	tasa = ((tamanio/(t_transcurrido/envios))/8)/1024;
+
+	printf("## |%9d| %6d | %13g | %12g |%12g|\n", tamanio, envios, t_transcurrido, t_transcurrido/envios, tasa);
+	return;
+}
--- pingpong.h
+++ pingpong.h
@@ -6,17 +6,18 @@
 #define ESCLAVO	1
 
 /* Tamaño de los datos a enviar */
-#define SIZE0	1024		/* 2^10 */
-#define SIZE1	16384		/* 2^14 */
-#define SIZE2	262144		/* 2^18 */
-#define SIZE3	1048576		/* 2^20 */
-#define SIZE4	1048576		/* 2^20 */
 
-#define SIZE5	524288		/* 2^19 */
-#define SIZE6	1048576		/* 2^20 */
-#define SIZE7	2097152		/* 2^21 */
-#define SIZE8	4194304		/* 2^22 */
-#define SIZE9	8388608		/* 2^22 */
+#define SIZE0	1024		/* 2^10 */
+#define SIZE1	4096		/* 2^12 */
+#define SIZE2	8192		/* 2^13 */
+#define SIZE3	16384		/* 2^14 */
+#define SIZE4	32768		/* 2^15 */
+
+#define SIZE5	65536		/* 2^16 */
+#define SIZE6	131072		/* 2^17 */
+#define SIZE7	262144		/* 2^18 */
+#define SIZE8	524288		/* 2^19 */
+#define SIZE9	1048576		/* 2^20 */
 
 /* Cantidad de envíos a hacer */
 #define ENVIOS	50
@@ -26,5 +27,9 @@
 			\n## 1)    100\
 			\n## 2)  1.000\
 			\n## 3) 10.000\n"
+
+/* Funciones */
+void obtener_info_sist();
+void master_func(int size, int envios, MPI_Status estado);
 
 #endif /* _PINGPONG_H_ */
